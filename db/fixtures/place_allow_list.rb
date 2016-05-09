e92d46b1389ea538e830b08747658b93fef10d8d
ShuzoN <d.namu.r21@gmail.com>
2016-05-10 04:16:57 +0900

    add: fixtures/PlaceAllowList, add seed data

---
+++ db/fixtures/place_allow_list.rb
@@ -0,0 +1,65 @@
+PlaceAllowList.seed( :id,
+  # 模擬店(食品販売)
+  { id: 1,  place_id: 1,  group_category_id: 1, enable: true  }, #事務棟エリア
+  { id: 2,  place_id: 2,  group_category_id: 1, enable: true  }, #図書館エリア
+  { id: 3,  place_id: 3,  group_category_id: 1, enable: true  }, #福利棟エリア
+  { id: 4,  place_id: 4,  group_category_id: 1, enable: true  }, #ステージエリア
+  { id: 5,  place_id: 5,  group_category_id: 1, enable: true  }, #体育館エリア
+  { id: 6,  place_id: 6,  group_category_id: 1, enable: false }, #セコムホール
+  { id: 7,  place_id: 7,  group_category_id: 1, enable: false }, #電気棟204
+  { id: 8,  place_id: 8,  group_category_id: 1, enable: false }, #電気棟206
+  { id: 9,  place_id: 9,  group_category_id: 1, enable: false }, #電気棟208
+  { id: 10, place_id: 10, group_category_id: 1, enable: false }, #電気棟212
+  { id: 11, place_id: 11, group_category_id: 1, enable: false }, #電気棟310
+  { id: 12, place_id: 12, group_category_id: 1, enable: false }, #講義棟部屋A
+  { id: 13, place_id: 13, group_category_id: 1, enable: false }, #講義棟部屋B
+  { id: 14, place_id: 14, group_category_id: 1, enable: false }, #マルチメディア
+  { id: 15, place_id: 15, group_category_id: 1, enable: false }, #グラウンド
+  # 模擬店(物品販売)
+  { id: 1,  place_id: 1,  group_category_id: 2, enable: true  }, #事務棟エリア
+  { id: 2,  place_id: 2,  group_category_id: 2, enable: true  }, #図書館エリア
+  { id: 3,  place_id: 3,  group_category_id: 2, enable: true  }, #福利棟エリア
+  { id: 4,  place_id: 4,  group_category_id: 2, enable: true  }, #ステージエリア
+  { id: 5,  place_id: 5,  group_category_id: 2, enable: true  }, #体育館エリア
+  { id: 6,  place_id: 6,  group_category_id: 2, enable: false }, #セコムホール
+  { id: 7,  place_id: 7,  group_category_id: 2, enable: false }, #電気棟204
+  { id: 8,  place_id: 8,  group_category_id: 2, enable: false }, #電気棟206
+  { id: 9,  place_id: 9,  group_category_id: 2, enable: false }, #電気棟208
+  { id: 10, place_id: 10, group_category_id: 2, enable: false }, #電気棟212
+  { id: 11, place_id: 11, group_category_id: 2, enable: false }, #電気棟310
+  { id: 12, place_id: 12, group_category_id: 2, enable: true  }, #講義棟部屋A
+  { id: 13, place_id: 13, group_category_id: 2, enable: true  }, #講義棟部屋B
+  { id: 14, place_id: 14, group_category_id: 2, enable: false }, #マルチメディア
+  { id: 15, place_id: 15, group_category_id: 2, enable: false }, #グラウンド
+  # 展示
+  { id: 1,  place_id: 1,  group_category_id: 4, enable: false }, #事務棟エリア
+  { id: 2,  place_id: 2,  group_category_id: 4, enable: false }, #図書館エリア
+  { id: 3,  place_id: 3,  group_category_id: 4, enable: false }, #福利棟エリア
+  { id: 4,  place_id: 4,  group_category_id: 4, enable: false }, #ステージエリア
+  { id: 5,  place_id: 5,  group_category_id: 4, enable: false }, #体育館エリア
+  { id: 6,  place_id: 6,  group_category_id: 4, enable: true  }, #セコムホール
+  { id: 7,  place_id: 7,  group_category_id: 4, enable: false }, #電気棟204
+  { id: 8,  place_id: 8,  group_category_id: 4, enable: false }, #電気棟206
+  { id: 9,  place_id: 9,  group_category_id: 4, enable: false }, #電気棟208
+  { id: 10, place_id: 10, group_category_id: 4, enable: false }, #電気棟212
+  { id: 11, place_id: 11, group_category_id: 4, enable: false }, #電気棟310
+  { id: 12, place_id: 12, group_category_id: 4, enable: true  }, #講義棟部屋A
+  { id: 13, place_id: 13, group_category_id: 4, enable: true  }, #講義棟部屋B
+  { id: 14, place_id: 14, group_category_id: 4, enable: true  }, #マルチメディア
+  { id: 15, place_id: 15, group_category_id: 4, enable: true  }, #グラウンド
+  # その他
+  { id: 1,  place_id: 1,  group_category_id: 5, enable: true }, #事務棟エリア
+  { id: 2,  place_id: 2,  group_category_id: 5, enable: true }, #図書館エリア
+  { id: 3,  place_id: 3,  group_category_id: 5, enable: true }, #福利棟エリア
+  { id: 4,  place_id: 4,  group_category_id: 5, enable: true }, #ステージエリア
+  { id: 5,  place_id: 5,  group_category_id: 5, enable: true }, #体育館エリア
+  { id: 6,  place_id: 6,  group_category_id: 5, enable: true }, #セコムホール
+  { id: 7,  place_id: 7,  group_category_id: 5, enable: true }, #電気棟204
+  { id: 8,  place_id: 8,  group_category_id: 5, enable: true }, #電気棟206
+  { id: 9,  place_id: 9,  group_category_id: 5, enable: true }, #電気棟208
+  { id: 10, place_id: 10, group_category_id: 5, enable: true }, #電気棟212
+  { id: 11, place_id: 11, group_category_id: 5, enable: true }, #電気棟310
+  { id: 12, place_id: 12, group_category_id: 5, enable: true }, #講義棟部屋A
+  { id: 13, place_id: 13, group_category_id: 5, enable: true }, #講義棟部屋B
+  { id: 14, place_id: 14, group_category_id: 5, enable: true }, #マルチメディア
+  { id: 15, place_id: 15, group_category_id: 5, enable: true }, #グラウンド
