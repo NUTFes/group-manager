2afd5b3de737757961dd9958902d4e9827582157
kkoike <k.koike03@gmail.com>
2016-08-31 19:56:53 +0900

    [add] 確認用シードデータの作成

---
+++ db/fixtures/development/9_assign_rental_item.rb
@@ -0,0 +1,23 @@
+AssignRentalItem.seed(:id,
+{
+    id: 10000010,
+    rental_order_id: 10000010,
+    rentable_item_id: 10000010,
+    num: 7,
+}, {
+    id: 10000011,
+    rental_order_id: 10000011,
+    rentable_item_id: 10000012,
+    num: 5,
+}, {
+    id: 10000012,
+    rental_order_id: 10000012,
+    rentable_item_id: 10000012,
+    num: 2,
+}, {
+    id: 10000013,
+    rental_order_id: 10000013,
+    rentable_item_id: 10000013,
+    num: 9,
+}
+)
