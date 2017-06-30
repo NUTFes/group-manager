968abcfa43f8c66ae02bb600d59b08c991a5f290
kkoike <k.koike03@gmail.com>
2017-06-30 22:33:56 +0900

    [upd] admin/stage_common_option, 団体を年度別で分けて表示するように．追加で団体をテキストで検索できるように

--- app/admin/stage_common_option.rb
+++ app/admin/stage_common_option.rb
@@ -41,5 +41,7 @@ ActiveAdmin.register StageCommonOption do
 
   preserve_default_filters!
   filter :fes_year
+  filter :group_name, as: :string
+  filter :group, label: "運営団体", as: :select, collection: proc {Group.active_admin_collection(3)} # 見やすくなるようにGroupを年度順にセパレータ付きで表示
 
 end
