0da26c23d6d7ea976cd1185a6f0cf3dbb6025188
kkoike <k.koike03@gmail.com>
2017-06-30 23:21:29 +0900

    [upd] admin/purchase_list, 団体を年度別で分けて表示するように．追加で団体をテキストで検索できるように

--- app/admin/purchase_list.rb
+++ app/admin/purchase_list.rb
@@ -40,5 +40,7 @@ ActiveAdmin.register PurchaseList do
 
   preserve_default_filters!
   filter :fes_year
+  filter :group_name, as: :string
+  filter :group, label: "運営団体", as: :select, collection: proc {Group.active_admin_collection(0)} # 見やすくなるようにGroupを年度順にセパレータ付きで表示
 
 end
