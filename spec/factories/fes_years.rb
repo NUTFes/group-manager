dcdf45e70edc8d181157950237247b4ba54ba0b0
kkoike <k.koike03@gmail.com>
2018-05-02 18:00:51 +0900

    [add] rails g rspec:model fes_year

---
+++ spec/factories/fes_years.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :fes_year do
+    
+  end
+end
---
+++ spec/models/fes_year_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe FesYear, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
