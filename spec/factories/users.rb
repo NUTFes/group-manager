3bc16375318ed98fac4ff818613d9c2b868a3b79
kkoike <k.koike03@gmail.com>
2018-05-02 23:16:09 +0900

    [add] factories/user, userモデルのテストデータ生成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,23 @@
 FactoryGirl.define do
   factory :user do
-    
+    sequence :email do |n|
+      "user#{n}@nutfes.com"
+    end
+    password 'nutfes'
+    password_confirmation 'nutfes'
+    role_id :role_user
+    to_create { |user| user.save(validate:false) } # validationを無効にする
+
+    initialize_with do
+      User.find_or_initialize_by(email: email)
+    end
+  end
+  factory :user_manager, parent: :user do
+    email 'manager@nutfes.com'
+    role_id :role_manager
+  end
+  factory :user_developer, parent: :user do
+    email 'developer@nutfes.com'
+    role_id :role_developer
   end
 end
