20781c3f0ff00ded55c4d5c88129609ccc944ea3
kkoike <k.koike03@gmail.com>
2018-05-02 23:15:27 +0900

    [add] factories/roles, roleモデルのテストデータ生成

--- spec/factories/roles.rb
+++ spec/factories/roles.rb
@@ -1,5 +1,20 @@
 FactoryGirl.define do
   factory :role do
-    
+    initialize_with do
+      Role.find_or_initialize_by(id: id, name: name)
+    end
+  end
+
+  factory :role_developer, parent: :role do
+    id 1
+    name 'developer'
+  end
+  factory :role_manager, parent: :role do
+    id 2
+    name 'manager'
+  end
+  factory :role_user, parent: :role do
+    id 3
+    name 'user'
   end
 end
