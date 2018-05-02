0d238ee446af30f215d63cde3f77bf97d8d68172
kkoike <k.koike03@gmail.com>
2018-05-02 23:18:55 +0900

    [add] factories/fes_year, groupfes_yearyモデルのテストデータ生成

--- spec/factories/fes_years.rb
+++ spec/factories/fes_years.rb
@@ -1,5 +1,9 @@
 FactoryGirl.define do
-  factory :fes_year do
-    
+  factory :this_year, class: FesYear do
+    fes_year Time.now.year
+
+    initialize_with do
+      FesYear.find_or_initialize_by(fes_year: fes_year)
+    end
   end
 end
