487e085e65709830b969c08d09c446420bef2210
kkoike <k.koike03@gmail.com>
2018-05-03 12:15:45 +0900

    [add] factories/power_order, power_orderモデルのテストデータ生成

--- spec/factories/power_orders.rb
+++ spec/factories/power_orders.rb
@@ -1,5 +1,9 @@
 FactoryGirl.define do
   factory :power_order do
-    association :group, factory: group
+    item 'テスト家電'
+    power 100
+    manufacturer 'テスト家電'
+    model '型番100-NF'
+    association :group, factory: :group_food
   end
 end
