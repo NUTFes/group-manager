950ee7a96c5ee1c8ac9ae476024d42defd207a32
kicchii <kicchi1.1618@gmail.com>
2018-05-04 13:33:10 +0900

    [upd] コントローラの編集

--- app/controllers/representative_contact_pages_controller.rb
+++ app/controllers/representative_contact_pages_controller.rb
@@ -1,2 +1,24 @@
 class RepresentativeContactPagesController < ApplicationController
+  def preview_pdf_page(template_name, output_file_name)
+    respond_to do |format|
+      format.pdf do
+        html = render_to_string template: "representative_contact_pages/#{template_name}"
+
+        pdf = PDFKit.new(html, encoding: "UTF-8")
+        
+        send_data pdf.to_pdf,
+          filename:    "参加団体連絡先_#{output_file_name}.pdf",
+          type:        "application/pdf",
+          disposition: "inline"
+       end
+    end
+  end
+
+  def representative_contact_sheet
+    this_year = FesYear.this_year
+
+    @groups = Group.year(this_year)
+    
+    preview_pdf_page('representative_contact_sheet', "代表者連絡先一覧")
+  end
 end
