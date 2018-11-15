a40d20b968b16867dc3ab73cef22df2b047f8b60
kkoike <k.koike03@gmail.com>
2018-11-15 13:39:32 +0900

    [mod] development環境のstocker_itemのseedデータからfes_yearを削除

--- db/fixtures/development/6_stocker_item.rb
+++ db/fixtures/development/6_stocker_item.rb
@@ -4,24 +4,20 @@ StockerItem.seed(:id,
     rental_item_id: 1,
     stocker_place_id: 1,
     num: 10,
-    fes_year_id: 2,
 }, {
     id: 10000011,
     rental_item_id: 1,
     stocker_place_id: 2,
     num: 5,
-    fes_year_id: 2,
 }, {
     id: 10000012,
     rental_item_id: 2,
     stocker_place_id: 3,
     num: 20,
-    fes_year_id: 2,
 }, {
     id: 10000013,
     rental_item_id: 3,
     stocker_place_id: 1,
     num: 10,
-    fes_year_id: 1,
 }
 )
