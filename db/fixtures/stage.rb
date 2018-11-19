af411cf637b3ffe8df2f272ad559f2e78fdec32b
youichiro <cinnamon416@gmail.com>
2018-11-20 01:35:20 +0900

    [upd] ステージ利用の申請に"希望なし"を用意

--- db/fixtures/stage.rb
+++ db/fixtures/stage.rb
@@ -1,8 +1,8 @@
 Stage.seed( :id,
   { id: 1 , name_ja: 'メインステージ'         , enable_sunny: true,  enable_rainy: false } ,
   { id: 2 , name_ja: 'サブステージ'           , enable_sunny: true,  enable_rainy: false } ,
-  { id: 3 , name_ja: '体育館'                 , enable_sunny: true,  enable_rainy: true  } ,
-  { id: 4 , name_ja: 'マルチメディアセンター' , enable_sunny: true,  enable_rainy: true  } ,
-  { id: 5 , name_ja: '武道館'                 , enable_sunny: true,  enable_rainy: true  } ,
-  { id: 6 , name_ja: 'なし'                   , enable_sunny: true,  enable_rainy: true  } ,
+  { id: 3 , name_ja: '体育館'                , enable_sunny: true,  enable_rainy: true  } ,
+  { id: 4 , name_ja: 'マルチメディアセンター'  , enable_sunny: true,  enable_rainy: true  } ,
+  { id: 5 , name_ja: '武道館'                , enable_sunny: true,  enable_rainy: true  } ,
+  { id: 6 , name_ja: '希望なし'              , enable_sunny: true,  enable_rainy: true  } ,
 )
