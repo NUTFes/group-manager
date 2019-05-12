4e6c4bb217b73ef67e2a39048c3f936f67affca5
kicchii <kicchi1.1618@gmail.com>
2019-05-12 23:56:28 +0900

    [fix] 選択肢と名前の修正

--- db/fixtures/inside_or_outside.rb
+++ db/fixtures/inside_or_outside.rb
@@ -1,5 +1,6 @@
 InsideOrOutside.seed( :id,
-  { id: 1, name: '未選択' },
-  { id: 2, name: '屋内'   },
-  { id: 3, name: '屋外'   },
+  { id: 1, name: '未選択'       },
+  { id: 2, name: '屋内'         },
+  { id: 3, name: '屋外'         },
+  { id: 4, name: 'どちらでも可' },
 )
