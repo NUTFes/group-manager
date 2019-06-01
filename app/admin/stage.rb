83cfb4b77b2fb11d9a8e95fc7b67c6777f93a1c3
youichiro <cinnamon416@gmail.com>
2019-06-01 16:22:06 +0900

    [upd] csv出力するカラムからcreated_at, updated_atを除く(2)

--- app/admin/stage.rb
+++ app/admin/stage.rb
@@ -2,14 +2,12 @@ ActiveAdmin.register Stage do
 
   permit_params :name_ja, :name_en, :enable_sunny, :enable_rainy
   
-    index do
+  index do
     id_column
-
     column :name_ja
     column :name_en
     column :enable_sunny
     column :enable_rainy
-
     actions
   end
 end
